cc02a29b4c7933a973a6cdfde0db06ea81194e80
nmkip <juan.ignacio848@gmail.com>
2018-09-20 14:44:07 -0300

    We can score a game where each roll is a zero.

---
+++ src/main/java/com/nmkip/bowlinggame/Game.java
@@ -0,0 +1,11 @@
+package com.nmkip.bowlinggame;
+
+public class Game {
+
+    public void roll(int pinsDown) {
+    }
+
+    public int score() {
+        return 0;
+    }
+}
--- src/test/java/com/nmkip/bowlinggame/BowlingGameTest.java
+++ src/test/java/com/nmkip/bowlinggame/BowlingGameTest.java
@@ -8,30 +8,10 @@ import static org.hamcrest.CoreMatchers.*;
 
 public class BowlingGameTest {
 
-    private BowlingGame bowlingGame;
-
-    @Before
-    public void setUp() throws Exception {
-        bowlingGame = new BowlingGame();
-    }
-
     @Test
-    public void canScoreGameOfZeros() {
-        int[] game = roll(0);
-        assertThat(bowlingGame.score(game), is(0));
-    }
-
-    @Test
-    public void canScoreGameOfOnes() {
-        int[] game = roll(1);
-        assertThat(bowlingGame.score(game), is(20));
-    }
-
-    private int[] roll(int scorePerGame) {
-        int[] game = new int[20];
-        for(int i = 0; i < 20; i++) {
-            game[i] = scorePerGame;
-        }
-        return game;
+    public void canScoreAGameOfZeros() {
+        Game game = new Game();
+        game.roll(0);
+        assertThat(game.score(), is(0));
     }
 }
