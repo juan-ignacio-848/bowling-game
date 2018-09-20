96480f89a35ce73758afeca2c8ed9065b8b5b92b
nmkip <juan.ignacio848@gmail.com>
2018-09-20 15:52:29 -0300

    End of kata.

--- src/main/java/com/nmkip/bowlinggame/Game.java
+++ src/main/java/com/nmkip/bowlinggame/Game.java
@@ -2,10 +2,46 @@ package com.nmkip.bowlinggame;
 
 public class Game {
 
+    int[] rolls = new int[21];
+    int lastRollIndex = 0;
+
+    public void roll(int... pinsDownPerFrame) {
+        for (int pinsDown : pinsDownPerFrame) {
+            roll(pinsDown);
+        }
+    }
+
     public void roll(int pinsDown) {
+        rolls[lastRollIndex] = pinsDown;
+        lastRollIndex++;
     }
 
     public int score() {
-        return 0;
+        int score = 0;
+        int cursor = 0;
+        final int numberOfFrames = 10;
+        for(int frame = 0; frame < numberOfFrames; frame++) {
+            if(isStrike(cursor)) {
+                score += 10 + rolls[cursor + 1] + rolls[cursor + 2];
+                cursor += 1;
+            } else if(isSpare(cursor)) {
+                score += 10 + rolls[cursor + 2];
+                cursor += 2;
+            } else {
+                score += rolls[cursor] + rolls[cursor + 1];
+                cursor += 2;
+            }
+        }
+        return score;
     }
+
+    private boolean isStrike(int cursor) {
+        return rolls[cursor] == 10;
+    }
+
+    private boolean isSpare(int cursor) {
+        return rolls[cursor] + rolls[cursor + 1] == 10;
+    }
+
+
 }
--- src/test/java/com/nmkip/bowlinggame/BowlingGameTest.java
+++ src/test/java/com/nmkip/bowlinggame/BowlingGameTest.java
@@ -3,15 +3,59 @@ package com.nmkip.bowlinggame;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BowlingGameTest {
 
+    private Game game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
     @Test
     public void canScoreAGameOfZeros() {
-        Game game = new Game();
-        game.roll(0);
+        game.roll(0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
         assertThat(game.score(), is(0));
     }
+
+    @Test
+    public void canScoreAGameOfOnes() {
+        game.roll(1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1);
+        assertThat(game.score(), is(20));
+    }
+
+    @Test
+    public void canScoreASpareFollowedBya3() {
+        game.roll(5,5, 3,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+        assertThat(game.score(), is(16));
+    }
+
+    @Test
+    public void canScoreAStrikeFollowedByA3AndA6() {
+        game.roll(10, 3,6, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+        assertThat(game.score(), is(28));
+    }
+
+    @Test
+    public void canScorePerfectGame() {
+        game.roll(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+        assertThat(game.score(), is(300));
+    }
+
+    @Test
+    public void acceptanceTest() {
+        game.roll(1,4, 4,5, 6,4, 5,5, 10, 0,1, 7,3, 6,4, 10, 2,8, 6);
+        assertThat(game.score(), is(133));
+    }
+
+    @Test
+    public void acceptanceTest2() {
+        game.roll(1,4, 4,5, 6,4, 5,5, 5,5, 0,1, 7,3, 6,4, 2,2, 2,8, 6);
+
+        assertThat(game.score(), is(103));
+    }
+
 }
