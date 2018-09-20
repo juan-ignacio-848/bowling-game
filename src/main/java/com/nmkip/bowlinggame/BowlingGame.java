5ab3838f0431999b98267c99bf5e6d01d58be67c
nmkip <juan.ignacio848@gmail.com>
2018-09-19 23:43:24 -0300

    Can score a game without strikes / spares.

--- src/main/java/com/nmkip/bowlinggame/BowlingGame.java
+++ src/main/java/com/nmkip/bowlinggame/BowlingGame.java
@@ -3,6 +3,10 @@ package com.nmkip.bowlinggame;
 public class BowlingGame {
 
     public int score(int[] game) {
-        return 0;
+        int score = 0;
+        for(int i = 0; i < game.length; i++) {
+            score += game[i];
+        }
+        return score;
     }
 }
