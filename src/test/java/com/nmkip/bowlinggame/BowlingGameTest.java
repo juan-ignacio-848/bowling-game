5018dac6bdbc128870c2b55d2fad269191f21233
nmkip <juan.ignacio848@gmail.com>
2018-09-22 18:45:25 -0300

    Removed duplication from tests.

--- src/test/java/com/nmkip/bowlinggame/BowlingGameTest.java
+++ src/test/java/com/nmkip/bowlinggame/BowlingGameTest.java
@@ -1,5 +1,6 @@
 package com.nmkip.bowlinggame;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -7,21 +8,30 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class BowlingGameTest {
 
+    private BowlingGame game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new BowlingGame();
+    }
+
     @Test
     public void should_score_0_when_every_roll_is_zero() {
-        BowlingGame game = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        final int times = 20;
+        final int pinsDown = 0;
+        roll(times, pinsDown);
         assertThat(game.score(), is(0));
     }
 
     @Test
     public void should_score_20_when_every_roll_is_one() {
-        BowlingGame game = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+        roll(20, 1);
         assertThat(game.score(), is(20));
+    }
+
+    private void roll(int times, int pinsDown) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pinsDown);
+        }
     }
 }
