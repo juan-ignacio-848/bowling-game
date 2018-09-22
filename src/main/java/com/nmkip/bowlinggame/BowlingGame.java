2c7689fd7ac2b2bea8234ed49aac5de9a9afe1cb
nmkip <juan.ignacio848@gmail.com>
2018-09-22 19:01:30 -0300

    We can handle calculating score for games where we roll a spare/strike followed by a non-spare/strike roll.

--- src/main/java/com/nmkip/bowlinggame/BowlingGame.java
+++ src/main/java/com/nmkip/bowlinggame/BowlingGame.java
@@ -2,13 +2,47 @@ package com.nmkip.bowlinggame;
 
 public class BowlingGame {
 
-    private int score = 0;
+    private final int NUMBER_OF_FRAMES = 10;
+    private int[] rolls = new int[21];
+    private int lastRollIndex = 0;
+
+    public void roll(int... rolls) {
+        for (int pinsDown : rolls) {
+            roll(pinsDown);
+        }
+    }
 
     public void roll(int pinsDown) {
-        score += pinsDown;
+        rolls[lastRollIndex] = pinsDown;
+        lastRollIndex += 1;
     }
 
     public int score() {
+        int score = 0;
+        int cursor = 0;
+
+        for(int frame = 0; frame < NUMBER_OF_FRAMES; frame++) {
+            if(isStrike(cursor)) {
+                score += 10 + rolls[cursor + 1] + rolls[cursor + 2];
+                cursor += 1;
+            } else if(isSpare(cursor)) {
+                score += 10 + rolls[cursor + 2];
+                cursor += 2;
+            } else {
+                score += rolls[cursor] + rolls[cursor + 1];
+                cursor += 2;
+            }
+        }
+
         return score;
     }
+
+    private boolean isSpare(int cursor) {
+        return rolls[cursor] + rolls[cursor + 1] == 10;
+    }
+
+    private boolean isStrike(int cursor) {
+        return rolls[cursor] == 10;
+    }
+
 }
--- src/test/java/com/nmkip/bowlinggame/BowlingGameTest.java
+++ src/test/java/com/nmkip/bowlinggame/BowlingGameTest.java
@@ -11,27 +11,32 @@ public class BowlingGameTest {
     private BowlingGame game;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         game = new BowlingGame();
     }
 
     @Test
     public void should_score_0_when_every_roll_is_zero() {
-        final int times = 20;
-        final int pinsDown = 0;
-        roll(times, pinsDown);
+        game.roll(0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
         assertThat(game.score(), is(0));
     }
 
     @Test
     public void should_score_20_when_every_roll_is_one() {
-        roll(20, 1);
+        game.roll(1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1);
         assertThat(game.score(), is(20));
     }
 
-    private void roll(int times, int pinsDown) {
-        for (int i = 0; i < times; i++) {
-            game.roll(pinsDown);
-        }
+    @Test
+    public void should_score_18_when_rolling_a_spare_followed_by_four_then_zeros() {
+        game.roll(5,5, 4,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+        assertThat(game.score(), is(18));
     }
+
+    @Test
+    public void should_score_24_when_rolling_a_strike_followed_by_four_three_then_zeros() {
+        game.roll(10, 4,3, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+        assertThat(game.score(), is(24));
+    }
+
 }
