bcbde4ce76ab3281057ab7a1636d2d47370abec6
nmkip <juan.ignacio848@gmail.com>
2018-09-22 00:02:20 -0300

    Extracted some methods and created a convenience method for rolling games.

--- src/test/java/com/nmkip/bowlinggame/BowlingGameTest.java
+++ src/test/java/com/nmkip/bowlinggame/BowlingGameTest.java
@@ -17,28 +17,20 @@ public class BowlingGameTest {
 
     @Test
     public void should_return_score_0_when_every_roll_is_0() {
-        roll(20, 0);
+        game.roll(0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
         assertThat(game.score(), is(0));
     }
 
     @Test
     public void should_return_score_20_when_every_roll_is_1() {
-        roll(20, 1);
+        game.roll(1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1);
         assertThat(game.score(), is(20));
     }
 
     @Test
     public void should_return_score_16_when_one_roll_is_spare_followed_by_3_and_then_0s() {
-        game.roll(5);
-        game.roll(5);
-        game.roll(3);
-        roll(17, 0);
+        game.roll(5,5, 3,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
         assertThat(game.score(), is(16));
     }
 
-    private void roll(int times, int pinsDown) {
-        for (int i = 0; i < times; i++) {
-            game.roll(pinsDown);
-        }
-    }
 }
