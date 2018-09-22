48e36d1fcc587b3d25f8c1d28c368eb04fe510b2
nmkip <juan.ignacio848@gmail.com>
2018-09-21 23:56:58 -0300

    We can handle calculating score for games where we roll a spare followed by a non-spare/strike roll.

--- src/main/java/com/nmkip/bowlinggame/Game.java
+++ src/main/java/com/nmkip/bowlinggame/Game.java
@@ -2,13 +2,27 @@ package com.nmkip.bowlinggame;
 
 public class Game {
 
-    private int score = 0;
+    private int[] rolls = new int[21];
+    private int lastRollIndex = 0;
 
     public void roll(int pinsDown) {
-        score += pinsDown;
+        rolls[lastRollIndex] = pinsDown;
+        lastRollIndex++;
     }
 
     public int score() {
+        int score = 0;
+        int cursor = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (rolls[cursor] + rolls[cursor + 1] == 10) {
+                score += 10 + rolls[cursor + 2];
+                cursor += 2;
+            } else {
+                score += rolls[cursor] + rolls[cursor + 1];
+                cursor += 2;
+            }
+        }
+
         return score;
     }
 }
