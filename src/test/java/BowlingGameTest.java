88583d72d7568447249f2a3ec663e04f248e8b66
nmkip <juan.ignacio848@gmail.com>
2018-11-11 22:21:29 -0300

    Refactored tests.

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,25 +1,33 @@
 import com.nmkip.bowlinggame.BowlingGame;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class BowlingGameTest {
 
+    private BowlingGame game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new BowlingGame();
+    }
+
     @Test
     public void should_score_0_when_rolling_a_game_of_0s() {
-        BowlingGame game = new BowlingGame();
-        for(int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        roll(20, 0);
         assertThat(game.score()).isEqualTo(0);
     }
 
     @Test
     public void should_score_20_when_rolling_a_game_of_1s() {
-        BowlingGame game = new BowlingGame();
-        for(int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+        roll(20, 1);
         assertThat(game.score()).isEqualTo(20);
+    }
+
+    private void roll(int times, int pinsDown) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pinsDown);
+        }
     }
 }
