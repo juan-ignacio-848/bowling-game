4d3d303fa82a8642355c2b098d7e60058fec923f
nmkip <juan.ignacio848@gmail.com>
2018-11-11 22:17:44 -0300

    Can score game of zeros.

---
+++ src/main/java/com/nmkip/bowlinggame/BowlingGame.java
@@ -0,0 +1,12 @@
+package com.nmkip.bowlinggame;
+
+public class BowlingGame {
+
+    public void roll(int pinsDown) {
+
+    }
+
+    public int score() {
+        return 0;
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,7 +1,16 @@
+import com.nmkip.bowlinggame.BowlingGame;
 import org.junit.Test;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class BowlingGameTest {
 
+    @Test
+    public void should_score_0_when_rolling_a_game_of_0s() {
+        BowlingGame game = new BowlingGame();
+        for(int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertThat(game.score()).isEqualTo(0);
+    }
 }
